0f1380a82aafe3853aeb699fbeafd85e2e51c848
Joel Koch <joel@joelkoch.dev>
2024-09-07 16:34:25 +0200

    Move files to correct directories

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Aliases
+    alias hm home-manager
+    alias hmsw 'home-manager switch --flake ~/nix'
+    alias l 'ls -lAh1'
+    alias lg lazygit
+    alias lsql lazysql
+    alias nixup 'nix flake update ~/nix'
+
+    # Interactive shell initialisation
+    set fish_greeting # Disable greeting
+
+    if type -q mise
+        ~/.local/bin/mise activate fish | source
+    end
+end
