619440bc87602e6e5426fd8627ddbe5d5482cbb4
Joel Koch <joel@joelkoch.dev>
2024-09-25 16:12:39 +0200

    Add macos config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,21 @@
 if status is-interactive
     # Aliases
-    alias hm home-manager
-    alias hmsw 'home-manager switch --flake ~/nix'
     alias l 'ls -lAh1'
     alias lg lazygit
     alias lsql lazysql
     alias y yazi
-    alias nixup 'nix flake update ~/nix'
+
+    # Environment
+    set -gx EDITOR hx
 
     # Interactive shell initialisation
     set fish_greeting # Disable greeting
 
     if type -q mise
         ~/.local/bin/mise activate fish | source
+    end
+
+    if type -q asdf
+        source ~/.asdf/asdf.fish
     end
 end
