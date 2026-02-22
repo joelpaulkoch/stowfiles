da40b34e981dbb8f153c7fde65a114ddc014a6d4
Joel Koch <joel@joelkoch.dev>
2026-02-22 22:01:54 +0100

    tide icon

--- fish/.config/fish/functions/tide/configure/icons.fish
+++ fish/.config/fish/functions/tide/configure/icons.fish
@@ -1,6 +1,7 @@
 tide_aws_icon  # Actual aws glyph is harder to see
+tide_bun_icon 󰳓
 tide_character_icon ❯
-tide_character_vi_icon_default ❮
+tide_character_vi_icon_default ❯
 tide_character_vi_icon_replace ▶
 tide_character_vi_icon_visual V
 tide_cmd_duration_icon
